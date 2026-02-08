9198fffe0ad7ba0a20b8c6ca6bbc33d14636437d
Aimad Harilla <59478784+imadharilla@users.noreply.github.com>
2026-02-08 15:12:19 +0100

    add logging pkg

---
+++ pkg/logging/logger.go
@@ -0,0 +1,14 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+)
+
+func ProdLogger() *slog.Logger {
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, Level: slog.LevelWarn}))
+}
+
+func DevLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, Level: slog.LevelDebug}))
+}
